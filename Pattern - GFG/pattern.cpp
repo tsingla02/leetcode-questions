63efd9efc6fdbde00734db9d08b75ea9ced76fea
tsingla02 <83027106+tsingla02@users.noreply.github.com>
2023-07-03 16:09:39 +0530

    Added solution - LeetHub

--- Pattern - GFG/pattern.cpp
+++ Pattern - GFG/pattern.cpp
@@ -6,35 +6,37 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
+    void printfirst(int n){
+        for(int i=0;i<n;i++){
+           for(int j=0;j < n-i-1;j++){
+               cout<<" ";
+           }
+           for(int j=0;j < i+1 ;j++){
+               cout<<"* ";
+           }
+        //   for(int j=n;j < n-i-1;j--){
+        //       cout<<" ";
+        //   }
+           cout<<endl;
+       }
+    }
+    void printlast(int n){
+        for(int i=0;i<n;i++){
+	        for(int j=0;j<i;j++){
+	            cout<<" ";
+	        }
+	        for(int j=0;j<=n-i-1;j++){
+                cout<<"* ";
+            }
+            // for(int j=n;j<i;j--){
+            //     cout<<" ";
+            // }
+            cout<<endl;
+        }
+    }
     void printDiamond(int n) {
-        // code here
-        int a = 1;
-        for(int i = 0 ; i< n ; i++)
-        {
-            for(int j = 0 ; j< n-i-1; j++)
-            {
-                cout<<' ';
-            }
-            for(int j = n-i-1 ; j< n ;j++)
-            {
-                cout<<'*';
-                if(j != n)cout<<' ';
-            }
-            cout<<endl;
-        }
-        for(int i = n-1 ; i >=0 ; i--)
-        {
-            for(int j = 0 ; j< n-i-1; j++)
-            {
-                cout<<' ';
-            }
-            for(int j = n-i-1 ; j< n ;j++)
-            {
-                cout<<'*';
-                if(j != n)cout<<' ';
-            }
-            cout<<endl;
-        }
+        printfirst(n);
+        printlast(n);
     }
 };
 
